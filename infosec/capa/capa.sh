1625624431a36fc94398b5d01a17e1ef10ba0a4b
SG <13872653+mmguero@users.noreply.github.com>
2020-07-16 15:35:31 -0600

    allow verbose or non-verbose running

--- infosec/capa/capa.sh
+++ infosec/capa/capa.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 ENCODING="utf-8"
-while getopts i: opts; do
+VERBOSE_FLAG=""
+while getopts i:v opts; do
    case ${opts} in
       i) IN_FILE=${OPTARG} ;;
+      v) VERBOSE_FLAG="-vv"
    esac
 done
 
 if [[ -z $IN_FILE ]] ; then
   echo "usage:"
-  echo "  capa.sh -i <IN_FILE>"
+  echo "  capa.sh -i <IN_FILE> [-v]"
   exit 1
 elif [[ ! -f "$IN_FILE" ]]; then
   echo "usage:"
-  echo "  capa.sh -i <IN_FILE>"
+  echo "  capa.sh -i <IN_FILE> [-v]"
   echo ""
   echo "$IN_FILE does not exist!"
   exit 1
@@ -32,4 +34,4 @@ cp "$IN_FILE" "$TEMP_DIR/"
 
 docker run --rm -t \
   -v "$TEMP_DIR:/data:rw" \
-  capa:latest "/data/$IN_BASENAME"
+  capa:latest "/data/$IN_BASENAME" $VERBOSE_FLAG
