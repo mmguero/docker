16d3b57ecb2178c906eaf3fc2f30aba9c76cc5c2
SG <13872653+mmguero@users.noreply.github.com>
2021-03-05 16:35:47 -0700

    udpates

--- net/nginx-securimage/docker/nginx.Dockerfile
+++ net/nginx-securimage/docker/nginx.Dockerfile
@@ -2,12 +2,18 @@ FROM trafex/alpine-nginx-php7:latest
 
 ENV WEBROOT /var/www/html
 
-ADD webcontent "$WEBROOT"
-
 RUN mkdir -p "$WEBROOT" && \
     cd "$WEBROOT" && \
     rm -f ./test.html && \
     mkdir ./securimage && \
-    curl -sSL "https://www.phpcaptcha.org/latest.tar.gz" | tar xzvf - -C ./securimage --strip-components 1
+    curl -ksSL "https://www.phpcaptcha.org/latest.tar.gz" | tar xzvf - -C ./securimage --strip-components 1 && \
+    curl -ksSL -o /tmp/story.zip "https://html5up.net/story/download" && \
+    unzip /tmp/story.zip -d "$WEBROOT" && \
+    rm -f /tmp/story.zip "$WEBROOT"/index*html "$WEBROOT"/README.txt
+
+ADD webcontent "$WEBROOT"
 
 EXPOSE 8080
+
+WORKDIR "$WEBROOT"
+
--- net/nginx-securimage/webcontent/index.php
+++ net/nginx-securimage/webcontent/index.php
@@ -12,67 +12,112 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
+
+  <title>The Parri Ward</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
   <link rel="stylesheet" href="/securimage/securimage.css" media="screen">
+  <link rel="stylesheet" href="/assets/css/main.css" />
   <style type="text/css">
   <!--
-  body { font-family: 'Helvetica', 'Arial', sans-serif; }
-  div.error { display: block; color: #f00; font-weight: bold; font-size: 1.2em; text-align:center; }
-  span.error { display: block; color: #f00; font-style: italic; text-align:center; }
-  .success { color: #00f; font-weight: bold; font-size: 1.2em; text-align:center; }
-  form label { display: block; font-weight: bold; }
-  fieldset { width: 50%; margin:auto; }
-  .note { font-size: 18px;
+    body { font-family: 'Helvetica', 'Arial', sans-serif; }
+    div.error { display: block; font-weight: bold; font-size: 1.2em; text-align:center; }
+    span.error { display: block; font-style: italic; text-align:center; }
+    .success {font-weight: bold; font-size: 1.2em; text-align:center; }
+    form label { display: block; font-weight: bold; }
+    fieldset { width: 70%; margin:auto; }
+    .note { font-size: 18px;
   -->
   </style>
+  <noscript><link rel="stylesheet" href="/assets/css/noscript.css" /></noscript>
 </head>
 
-<body>
-<fieldset>
+<body class="is-preload">
 
-<?php
-process_si_verify_form();
-if (isset($_SESSION['ctform']['error']) &&  $_SESSION['ctform']['error'] == true):              /* form submission had errors */ ?>
-<div class="error">Incorrect CAPTCHA, please try again.</div><br>
-<?php endif; ?>
+  <div id="wrapper" class="divided">
 
-<?php if (isset($_SESSION['ctform']['success']) && $_SESSION['ctform']['success'] == true):     /* humanity verified, reveal the goods */ ?>
+    <!-- One -->
+    <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+      <div class="content">
 
-<div class="success">The CAPTCHA was solved in <?php echo $_SESSION['ctform']['timetosolve'] ?> seconds.</div><br />
+        <?php
+        process_si_verify_form();
 
-<?php else: /* only show CAPTCHA if not already verified (error or first time here) */ ?>
+        /* if the form submission had errors... */
+        if (isset($_SESSION['ctform']['error']) &&  $_SESSION['ctform']['error'] == true): ?>
+          <div class="error">The text did not match. Please try again.<br /></div>
+        <?php endif; ?>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']) ?>" id="verify_form">
+        <?php if (isset($_SESSION['ctform']['success']) && $_SESSION['ctform']['success'] == true):     /* humanity verified, reveal the goods */ ?>
 
-  <input type="hidden" name="do" value="verifyhumanity">
+            <h1>Join us.</h1>
+            <p class="major">Welcome to ACME.</p>
+            <ul class="actions stacked">
+              <li><a href="#one" class="button big wide smooth-scroll-middle">Department one</a></li>
+              <li><a href="#two" class="button big wide smooth-scroll-middle">Department two</a></li>
+              <li><a href="#three" class="button big wide smooth-scroll-middle">Department three</a></li>
+              <li><a href="#four" class="button big wide smooth-scroll-middle">Department four</a></li>
+            </ul>
 
-  <div>
-    <?php
-      // display CAPTCHA form
-      require_once 'securimage/securimage.php';
-      $options = array();
-      $options['input_name']             = 'ct_captcha'; // change name of input element for form post
-      $options['disable_flash_fallback'] = true;         // don't allow flash fallback
-      if (!empty($_SESSION['ctform']['captcha_error'])) {
-        // error html to show in captcha output
-        $options['error_html'] = $_SESSION['ctform']['captcha_error'];
-      }
-      echo "<div id='captcha_container_1'>\n";
-      echo Securimage::getCaptchaHtml($options);
-      echo "\n</div>\n";
-    ?>
-  </div>
+        <?php else: /* only show CAPTCHA if not already verified (error or first time here) */ ?>
+          <fieldset>
+            <div class="error">Help us stay secure by retyping the text from the picture.<br /></div>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']) ?>" id="verify_form">
 
-  <p>
-    <input type="submit" value="I am not a robot">
-  </p>
+              <input type="hidden" name="do" value="verifyhumanity">
 
-</form>
+              <div>
+                <?php
+                  // display CAPTCHA form
+                  require_once 'securimage/securimage.php';
+                  $options = array();
+                  $options['input_name']             = 'ct_captcha'; // change name of input element for form post
+                  $options['disable_flash_fallback'] = true;         // don't allow flash fallback
+                  if (!empty($_SESSION['ctform']['captcha_error'])) {
+                    // error html to show in captcha output
+                    $options['error_html'] = $_SESSION['ctform']['captcha_error'];
+                  }
+                  echo "<div id='captcha_container_1'>\n";
+                  echo Securimage::getCaptchaHtml($options);
+                  echo "\n</div>\n";
+                ?>
+              </div>
 
-<?php endif; /* end if block to display captcha */ ?>
+              <p>
+                <input type="submit" value="I am not a robot">
+              </p>
 
-</fieldset>
+            </form>
+          </fieldset>
+
+        <?php endif; /* end if block to display captcha */ ?>
+
+      </div> <!-- content -->
+
+      <div class="image">
+        <img src="images/banner.jpg" alt="" />
+      </div>
+
+    </section> <!-- One -->
+
+    <!-- Footer -->
+    <footer class="wrapper style1 align-center">
+      <div class="inner">
+        <p>site template: <a href="https://html5up.net">HTML5 UP</a>.</p>
+      </div>
+    </footer>
+
+  </div> <!-- wrapper divided -->
+
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrollex.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+
 </body>
 </html>
 
@@ -109,7 +154,7 @@ function process_si_verify_form() {
       require_once dirname(__FILE__) . '/securimage/securimage.php';
       $securimage = new Securimage();
       if ($securimage->check($captcha) == false) {
-        $errors['captcha_error'] = 'Incorrect CAPTCHA, please try again.<br />';
+        $errors['captcha_error'] = 'Incorrect CAPTCHA<br />';
       }
 
     }
