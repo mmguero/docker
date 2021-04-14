f106567b7e27db2832573ec7967db8758525170c
SG <13872653+mmguero@users.noreply.github.com>
2021-04-14 08:53:34 -0600

    Fix docker file script usage

--- net/zeek/zeek-docker.sh
+++ net/zeek/zeek-docker.sh
@@ -2,8 +2,11 @@
 
 set -u
 set -o pipefail
+shopt -s nullglob
 
 ENCODING="utf-8"
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
 export ZEEK_DOCKER_IMAGE=mmguero/zeek:latest
 
@@ -25,19 +28,22 @@ SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
 # with "local", then the default "local" policy will not be used.
 pushd $SCRIPT_PATH >/dev/null 2>&1
 LOCAL_SCRIPT=local
-LOCAL_ZEEK_ARGS=()
-for LOCAL_ZEEK_FILE in *.zeek; do
-  LOCAL_ZEEK_ARG="$($BASENAME "$LOCAL_ZEEK_FILE")"
-  LOCAL_ZEEK_ARGS+=( "$LOCAL_ZEEK_ARG" )
-  [[ "$LOCAL_ZEEK_ARG" =~ ^local ]] && LOCAL_SCRIPT=
+LOCAL_ZEEK_SCRIPTS=()
+for FILE in *.zeek; do
+  if [[ -r "$FILE" ]]; then
+    LOCAL_ZEEK_SCRIPT="$($BASENAME "$FILE")"
+    LOCAL_ZEEK_SCRIPTS+=( "$LOCAL_ZEEK_SCRIPT" )
+    [[ "$LOCAL_ZEEK_SCRIPT" =~ ^local ]] && LOCAL_SCRIPT=
+  fi
 done
 popd >/dev/null 2>&1
 
 export REALPATH
 export DIRNAME
 export BASENAME
+export SCRIPT_PATH
 export LOCAL_SCRIPT
-export LOCAL_ZEEK_ARGS
+export LOCAL_ZEEK_ARGV="$(join_by ':' "${LOCAL_ZEEK_SCRIPTS[@]}")"
 export DEFAULT_UID=$(id -u)
 export DEFAULT_GID=$(id -g)
 
@@ -79,9 +85,10 @@ printf "%s\0" "$@" | $XARGS -0 -n 1 -P ${MAX_ZEEK_PROCS:-4} -I XXX bash -c '
   fi
 
   # create a read-only mount for each local zeek script
-  for LOCAL_ZEEK_ARG in "${LOCAL_ZEEK_ARGS[@]}"; do
+  IFS=":" read -r -a ZEEK_PARAMS <<< "$LOCAL_ZEEK_ARGV"
+  for ZEEK_PARAM in "${ZEEK_PARAMS[@]}"; do
     MOUNT_ARGS+=( -v )
-    MOUNT_ARGS+=( "$SCRIPT_PATH/$LOCAL_ZEEK_ARG:/opt/zeek/share/zeek/site/$LOCAL_ZEEK_ARG:ro" )
+    MOUNT_ARGS+=( "$SCRIPT_PATH/$ZEEK_PARAM:/opt/zeek/share/zeek/site/$ZEEK_PARAM:ro" )
   done
 
   # each instance of zeek will write to its own log directory
@@ -93,5 +100,5 @@ printf "%s\0" "$@" | $XARGS -0 -n 1 -P ${MAX_ZEEK_PROCS:-4} -I XXX bash -c '
   # run zeek in docker on the provided input
   docker run --rm $NETWORK_MODE -e DEFAULT_UID=$DEFAULT_UID -e DEFAULT_GID=$DEFAULT_GID \
     "${CAP_ARGS[@]}" "${MOUNT_ARGS[@]}" $ZEEK_DOCKER_IMAGE \
-    $ZEEK_EXE -C $IN_FLAG $LOCAL_SCRIPT "${LOCAL_ZEEK_ARGS[@]}"
+    $ZEEK_EXE -C $IN_FLAG $LOCAL_SCRIPT "${ZEEK_PARAMS[@]}"
 '
