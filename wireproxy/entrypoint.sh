02b94a0fd61e859a1fcd30056e3017208b508ac6
SG <13872653+mmguero@users.noreply.github.com>
2022-03-30 09:28:55 -0600

    fix entrypoint command

--- wireproxy/entrypoint.sh
+++ wireproxy/entrypoint.sh
@@ -4,4 +4,4 @@ if [ -n "$WIREPROXY_CONFIG_FROM_ENVS" ] && [ "$WIREPROXY_CONFIG_FROM_ENVS" == "t
   /usr/bin/envsubst < /etc/wireproxy/config.env > /etc/wireproxy/config
 fi
 
-/usr/bin/wireproxy /etc/wireproxy/config
+/usr/bin/wireproxy -c /etc/wireproxy/config
