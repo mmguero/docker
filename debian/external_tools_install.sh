690ccb587613d866675af3942e9f5c0bd619564f
Seth Grover <mero.mero.guero@gmail.com>
2022-11-01 15:37:08 -0600

    external tool setup

---
+++ debian/external_tools_install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+set -x
+
+DEB_ARCH=$(dpkg --print-architecture)
+LINUX_CPU=$(uname -m)
+
+if [ $DEB_ARCH = arm* ]; then
+  if [ $LINUX_CPU = aarch64 ]; then
+    STARSHIP_RELEASE_FILENAME=starship-aarch64-unknown-linux-musl.tar.gz
+  else
+    STARSHIP_RELEASE_FILENAME=starship-arm-unknown-linux-musleabihf.tar.gz
+  fi
+else
+  STARSHIP_RELEASE_FILENAME=starship-x86_64-unknown-linux-gnu.tar.gz
+fi
+
+cd /tmp
+curl -o ./starship.tar.gz -sSL "https://github.com/starship/starship/releases/latest/download/$STARSHIP_RELEASE_FILENAME"
+tar xf ./starship.tar.gz
+mv ./starship /usr/bin/starship
+chmod 755 /usr/bin/starship
+
+curl -o ./getcroc.sh -sSL "https://getcroc.schollz.com"
+chmod +x ./getcroc.sh
+./getcroc.sh -p /usr/bin
