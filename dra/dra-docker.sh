2d374c9c59645b7197c7c9f03ed17566f244837e
SG <mero.mero.guero@gmail.com>
2022-11-07 09:19:41 -0700

    fix temp dir for dra

--- dra/dra-docker.sh
+++ dra/dra-docker.sh
@@ -9,7 +9,8 @@ else
   CONTAINER_PGID=$(id -g)
 fi
 
-TEMP_DIR=$(mktemp -d -t dra.XXXXXXXXXX)
+TEMP_DIR="$(mktemp -d -t dra.XXXXXXXXXX)"
+TEMP_DIR_BASENAME="$(basename "$TEMP_DIR")"
 
 function finish {
   rm -rf "$TEMP_DIR"
@@ -19,8 +20,8 @@ trap finish EXIT
 $ENGINE run -i -t --rm \
   -e PUID=$CONTAINER_PUID \
   -e PGID=$CONTAINER_PGID \
-  -v "$TEMP_DIR:$TEMP_DIR:rw" \
-  -w "$TEMP_DIR" \
+  -v "$TEMP_DIR:/tmp/$TEMP_DIR_BASENAME:rw" \
+  -w "/tmp/$TEMP_DIR_BASENAME" \
   ghcr.io/mmguero/dra "$@"
 
 mv "$TEMP_DIR"/* ./ >/dev/null 2>&1 || true
