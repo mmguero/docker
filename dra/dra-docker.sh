5799fcf8ef5d4c2865e570b5d8639b63bdf2d5a8
SG <mero.mero.guero@gmail.com>
2022-11-06 21:59:42 -0700

    dra-docker.sh

---
+++ dra/dra-docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ENGINE="${CONTAINER_ENGINE:-docker}"
+if [[ "$ENGINE" == "podman" ]]; then
+  CONTAINER_PUID=0
+  CONTAINER_PGID=0
+else
+  CONTAINER_PUID=$(id -u)
+  CONTAINER_PGID=$(id -g)
+fi
+
+DIR="$(pwd)"
+
+$ENGINE run -i -t --rm \
+  -e PUID=$CONTAINER_PUID \
+  -e PGID=$CONTAINER_PGID \
+  -v "$DIR:$DIR:rw" \
+  -w "$DIR" \
+  ghcr.io/mmguero/dra "$@"
