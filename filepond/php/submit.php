b56a0d64fbffa7800d1a4c3c5558b407b458d795
SG <mero.mero.guero@gmail.com>
2023-08-09 06:59:57 -0600

    playing with filepond

--- filepond/php/submit.php
+++ filepond/php/submit.php
@@ -1,19 +1,15 @@
 <?php
 
-// Comment if you don't want to allow posts from other domains
 header('Access-Control-Allow-Origin: *');
-
-// Allow the following methods to access this file
 header('Access-Control-Allow-Methods: POST');
 
-// Load the FilePond class
 require_once('FilePond.class.php');
 
-// Load our configuration for this server
 require_once('config.php');
 
 // Catch server exceptions and auto jump to 500 response code if caught
 FilePond\catch_server_exceptions();
+
 FilePond\route_form_post(ENTRY_FIELD, [
     'FILE_OBJECTS' => 'handle_file_post',
     'BASE64_ENCODED_FILE_OBJECTS' => 'handle_base64_encoded_file_post',
@@ -53,13 +49,10 @@ function move_file_prefixed($file, $path, $prefix) {
 
 function handle_file_post($files) {
 
-    // This is a very basic implementation of a classic PHP upload function, please properly
-    // validate all submitted files before saving to disk or database, more information here
-    // http://php.net/manual/en/features.file-upload.php
-    
     foreach($files as $file) {
         FilePond\move_file($file, UPLOAD_DIR);
     }
+
 }
 
 function handle_base64_encoded_file_post($files) {
@@ -78,13 +71,8 @@ function handle_base64_encoded_file_post($files) {
         }
         */
         $file = @json_decode($file);
-
-        // Skip files that failed to decode
         if (!is_object($file)) continue;
 
-
-
-        // write file to disk
         FilePond\write_file(
             UPLOAD_DIR, 
             base64_decode($file->data), 
@@ -98,10 +86,7 @@ function handle_transfer_ids_post($ids) {
 
     foreach ($ids as $id) {
         
-        // create transfer wrapper around upload
         $transfer = FilePond\get_transfer(TRANSFER_DIR, $id);
-
-        // transfer not found
         if (!$transfer) continue;
 
         $new_name_prefix = '';
@@ -113,18 +98,24 @@ function handle_transfer_ids_post($ids) {
             }
         }
         
-        // move files
         $files = $transfer->getFiles(defined('TRANSFER_PROCESSOR') ? TRANSFER_PROCESSOR : null);
-
         if($files != null){
            foreach($files as $file) {
                 move_file_prefixed($file, UPLOAD_DIR, $new_name_prefix);
             } 
         }
-        
 
-        // remove transfer directory
         FilePond\remove_transfer_directory(TRANSFER_DIR, $id);
     }
+
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        $return_to = $_SERVER['HTTP_REFERER'];
+        console_log("referrer page", true);
+    } else {
+        $return_to = "/upload";
+        console_log("fallback", true);
+    }
+    console_log($return_to, true);
+    # header("Location: ". $return_to);
 
 }
