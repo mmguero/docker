fc7c27fb607e9d5573133e1c1a843ae15a5b02aa
SG <mero.mero.guero@gmail.com>
2023-08-14 13:34:30 -0600

    fix wireproxy shell scripts

--- wireproxy/entrypoint.sh
+++ wireproxy/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -n "$WIREPROXY_CONFIG_FROM_ENVS" ] && [ "$WIREPROXY_CONFIG_FROM_ENVS" == "true" ]; then
+if [[ -n "$WIREPROXY_CONFIG_FROM_ENVS" ]] && [[ "$WIREPROXY_CONFIG_FROM_ENVS" == "true" ]]; then
   /usr/bin/envsubst < /etc/wireproxy/config.env > /etc/wireproxy/config
 fi
 
