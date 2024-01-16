e3354373982757943404c07f269db3a004f4d915
Seth Grover <mero.mero.guero@gmail.com>
2024-01-16 12:41:20 -0700

    minor updates

--- ctfd/nginx/certs/gen_self_signed_certs.sh
+++ ctfd/nginx/certs/gen_self_signed_certs.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+if [ -z "$BASH_VERSION" ]; then
+  echo "Wrong interpreter, please run \"$0\" with bash"
+  exit 1
+fi
+
+set -e
+
+# force-navigate to base directory (parent of scripts/ directory)
+[[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
+[[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
+if ! (type "$REALPATH" && type "$DIRNAME") > /dev/null; then
+  echo "$(basename "${BASH_SOURCE[0]}") requires $REALPATH and $DIRNAME"
+  exit 1
+fi
+SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
+pushd "$SCRIPT_PATH" >/dev/null 2>&1
+
 # one may wish to consider not using self-signed certificates in production
 openssl dhparam -out dhparam.pem 2048
 openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 3650
+
+popd >/dev/null 2>&1
