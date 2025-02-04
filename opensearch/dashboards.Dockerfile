a12f66a47891099750bd118b4637004c47176222
Seth Grover <seth.d.grover@gmail.com>
2025-02-04 13:23:37 -0700

    opensearch build in default ecs template

--- opensearch/dashboards.Dockerfile
+++ opensearch/dashboards.Dockerfile
@@ -13,6 +13,8 @@ ENV TERM xterm
 ENV TINI_VERSION v0.19.0
 ENV TINI_URL https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini
 
+ENV ECS_RELEASES_URL "https://api.github.com/repos/elastic/ecs/releases/latest"
+
 ENV OSD_TRANSFORM_VIS_VERSION 2.18.0
 
 USER root
@@ -23,7 +25,7 @@ ADD https://github.com/lguillaud/osd_transform_vis/releases/download/$OSD_TRANSF
 
 RUN export BINARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/') && \
     yum upgrade -y && \
-    yum install -y curl-minimal psmisc findutils util-linux openssl rsync python3 zip unzip && \
+    yum install -y curl-minimal psmisc findutils util-linux openssl jq rsync python3 zip unzip && \
     curl -sSLf -o /usr/bin/tini "${TINI_URL}-${BINARCH}" && \
     usermod -a -G tty ${PUSER} && \
     cd /tmp && \
@@ -34,11 +36,26 @@ RUN export BINARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/') 
         cd /usr/share/opensearch-dashboards/plugins && \
         /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin install file:///tmp/transformVis.zip --allow-root && \
         rm -rf /tmp/transformVis /tmp/opensearch-dashboards && \
+    mkdir -p /opt/ecs /data/init && \
+      cd /opt && \
+      curl -sSL "$(curl -sSL "$ECS_RELEASES_URL" | jq '.tarball_url' | tr -d '"')" | tar xzf - -C ./ecs --strip-components 1 && \
+      mv /opt/ecs/generated/elasticsearch /opt/ecs-templates && \
+      mv /opt/ecs-templates/ /opt/ecs-templates-os/ && \
+      find /opt/ecs-templates-os -name "*.json" -exec sed -i 's/\("type"[[:space:]]*:[[:space:]]*\)"match_only_text"/\1"text"/' "{}" \; && \
+      find /opt/ecs-templates-os -name "*.json" -exec sed -i 's/\("type"[[:space:]]*:[[:space:]]*\)"constant_keyword"/\1"keyword"/' "{}" \; && \
+      find /opt/ecs-templates-os -name "*.json" -exec sed -i 's/\("type"[[:space:]]*:[[:space:]]*\)"wildcard"/\1"keyword"/' "{}" \; && \
+      find /opt/ecs-templates-os -name "*.json" -exec sed -i 's/\("type"[[:space:]]*:[[:space:]]*\)"flattened"/\1"nested"/' "{}" \; && \
+      find /opt/ecs-templates-os -name "*.json" -exec sed -i 's/\("type"[[:space:]]*:[[:space:]]*\)"number"/\1"long"/' "{}" \; && \
+      find /opt/ecs-templates-os -name "*.json" -exec bash -c "jq 'walk(if type == \"object\" and has(\"synthetic_source_keep\") then del(.synthetic_source_keep) else . end)' \"{}\" > \"{}\".new && mv \"{}\".new \"{}\"" \; && \
+      rm -rf /opt/ecs && \
     mkdir -p /var/local/ca-trust && \
-    chown --silent -R ${PUSER}:${PGROUP} /usr/share/opensearch-dashboards /var/local/ca-trust && \
+    chown --silent -R ${PUSER}:${PGROUP} /usr/share/opensearch-dashboards /var/local/ca-trust /opt/ecs-templates-os /data/init && \
     chmod 755 /usr/bin/tini /usr/local/bin/*.sh && \
     yum clean all && \
     rm -rf /var/cache/yum
+
+ADD shared-objects/templates /opt/templates
+ADD shared-objects/scripts /usr/local/bin
 
 VOLUME ["/var/local/ca-trust"]
 
---
+++ opensearch/shared-objects/scripts/shared-object-creation.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+
+set -euo pipefail
+shopt -s nocasematch
+
+DASHB_URL=${DASHBOARDS_URL:-"https://localhost:5601"}
+INDEX_PATTERN=${DEFAULT_INDEX_PATTERN:-"ecs-*"}
+INDEX_TIME_FIELD=${INDEX_TIME_FIELD:-"@timestamp"}
+DARK_MODE=${DASHBOARDS_DARKMODE:-"true"}
+
+TEMPLATES_DIR="/opt/templates"
+TEMPLATE_FILE_ORIG="$TEMPLATES_DIR/ecs_template.json"
+TEMPLATE_FILE="/data/init/ecs_template.json"
+
+STARTUP_IMPORT_PERFORMED_FILE=/tmp/shared-objects-created
+
+if [[ -n "$OPENSEARCH_HOSTS" ]]; then
+  readarray -t OPENSEARCH_ARRAY < <(echo "$OPENSEARCH_HOSTS" | jq -r '.[]')
+  OPENSEARCH_URL_TO_USE="${OPENSEARCH_ARRAY[0]}"
+else
+  OPENSEARCH_URL_TO_USE="http://opensearch-node1:9200"
+fi
+
+OPENSEARCH_CREDS_CONFIG_FILE_TO_USE=${OPENSEARCH_CREDS_CONFIG_FILE:-"/var/local/curlrc/.creds.curlrc"}
+if [[ -r "$OPENSEARCH_CREDS_CONFIG_FILE_TO_USE" ]]; then
+  CURL_CONFIG_PARAMS=(
+    --config
+    "$OPENSEARCH_CREDS_CONFIG_FILE_TO_USE"
+    )
+else
+  CURL_CONFIG_PARAMS=()
+fi
+
+DASHBOARDS_URI_PATH="opensearch-dashboards"
+XSRF_HEADER="osd-xsrf"
+ECS_TEMPLATES_DIR=/opt/ecs-templates-os
+
+# is the Dashboards process server up and responding to requests?
+if curl "${CURL_CONFIG_PARAMS[@]}" -fsSL -XGET "$DASHB_URL/api/status" ; then
+
+  #############################################################################################################################
+  # Templates
+  #   - a sha256 sum of the combined templates is calculated and the templates are imported if the previously stored hash
+  #     (if any) does not match the files we see currently.
+
+  TEMPLATES_IMPORTED=false
+  TEMPLATES_IMPORT_DIR="$(mktemp -d -t templates-XXXXXX)"
+  rsync -a "$TEMPLATES_DIR"/ "$TEMPLATES_IMPORT_DIR"/
+  TEMPLATE_FILE_ORIG_TMP="$(echo "$TEMPLATE_FILE_ORIG" | sed "s@$TEMPLATES_DIR@$TEMPLATES_IMPORT_DIR@")"
+
+  # calculate combined SHA sum of all templates to save as _meta.hash to determine if
+  # we need to do this import (mostly useful for the secondary loop)
+  TEMPLATE_HASH="$(find "$ECS_TEMPLATES_DIR"/composable "$TEMPLATES_IMPORT_DIR" -type f -name "*.json" -size +2c 2>/dev/null | sort | xargs -r cat | sha256sum | awk '{print $1}')"
+
+  # get the previous stored template hash (if any) to avoid importing if it's already been imported
+  set +e
+  TEMPLATE_HASH_OLD="$(curl "${CURL_CONFIG_PARAMS[@]}" -fsSL -XGET -H "Content-Type: application/json" "$OPENSEARCH_URL_TO_USE/_index_template/ecs_template" 2>/dev/null | jq --raw-output '.index_templates[]|select(.name=="ecs_template")|.index_template._meta.hash' 2>/dev/null)"
+  set -e
+
+  # proceed only if the current template HASH doesn't match the previously imported one, or if there
+  # was an error calculating or storing either
+  if [[ "$TEMPLATE_HASH" != "$TEMPLATE_HASH_OLD" ]] || [[ -z "$TEMPLATE_HASH_OLD" ]] || [[ -z "$TEMPLATE_HASH" ]]; then
+
+    if [[ -d "$ECS_TEMPLATES_DIR"/composable/component ]]; then
+      echo "Importing ECS composable templates..."
+      for i in "$ECS_TEMPLATES_DIR"/composable/component/*.json; do
+        TEMP_BASENAME="$(basename "$i")"
+        TEMP_FILENAME="${TEMP_BASENAME%.*}"
+        echo "Importing ECS composable template $TEMP_FILENAME ..."
+        curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" \
+          "$OPENSEARCH_URL_TO_USE/_component_template/ecs_$TEMP_FILENAME" -d "@$i" 2>&1
+      done
+    fi
+
+    if [[ -d "$TEMPLATES_IMPORT_DIR"/composable/component ]]; then
+      echo "Importing custom ECS composable templates..."
+      for i in "$TEMPLATES_IMPORT_DIR"/composable/component/*.json; do
+        TEMP_BASENAME="$(basename "$i")"
+        TEMP_FILENAME="${TEMP_BASENAME%.*}"
+        echo "Importing custom ECS composable template $TEMP_FILENAME ..."
+        curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" \
+          "$OPENSEARCH_URL_TO_USE/_component_template/custom_$TEMP_FILENAME" -d "@$i" 2>&1
+      done
+    fi
+
+    echo "Importing ecs_template ($TEMPLATE_HASH)..."
+
+    if [[ -f "$TEMPLATE_FILE_ORIG_TMP" ]] && [[ ! -f "$TEMPLATE_FILE" ]]; then
+      cp "$TEMPLATE_FILE_ORIG_TMP" "$TEMPLATE_FILE"
+    fi
+
+    # store the TEMPLATE_HASH we calculated earlier as the _meta.hash for the ecs template
+    TEMPLATE_FILE_TEMP="$(mktemp)"
+    ( jq "._meta.hash=\"$TEMPLATE_HASH\"" "$TEMPLATE_FILE" >"$TEMPLATE_FILE_TEMP" 2>/dev/null ) && \
+      [[ -s "$TEMPLATE_FILE_TEMP" ]] && \
+      cp -f "$TEMPLATE_FILE_TEMP" "$TEMPLATE_FILE" && \
+      rm -f "$TEMPLATE_FILE_TEMP"
+
+    # load ecs_template containing field type mappings (merged from /opt/templates/ecs_template.json to /data/init/ecs_template.json on startup)
+    curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" \
+      "$OPENSEARCH_URL_TO_USE/_index_template/ecs_template" -d "@$TEMPLATE_FILE" 2>&1
+
+    # import other templates as well
+    for i in "$TEMPLATES_IMPORT_DIR"/*.json; do
+      TEMP_BASENAME="$(basename "$i")"
+      TEMP_FILENAME="${TEMP_BASENAME%.*}"
+      if [[ "$TEMP_FILENAME" != "ecs_template" ]]; then
+        echo "Importing template \"$TEMP_FILENAME\"..."
+        curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" \
+          "$OPENSEARCH_URL_TO_USE/_index_template/$TEMP_FILENAME" -d "@$i" 2>&1
+      fi
+    done
+
+    TEMPLATES_IMPORTED=true
+
+  else
+    echo "ecs_template ($TEMPLATE_HASH) already exists ($LOOP) at \"${OPENSEARCH_URL_TO_USE}\""
+  fi # TEMPLATE_HASH check
+
+  rm -rf "${TEMPLATES_IMPORT_DIR}"
+
+  # end Templates
+  #############################################################################################################################
+
+  #############################################################################################################################
+  # Index pattern(s)
+  #   - Only set overwrite=true if we actually updated the templates above, otherwise overwrite=false and fail silently
+  #     if they already exist (http result code 409)
+  echo "Importing index pattern..."
+
+  # Save off any custom field formatting prior to an overwrite
+  FIELD_FORMAT_MAP_FILE_TEMP="$(mktemp)"
+  ( curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XGET -H "Content-Type: application/json" -H "$XSRF_HEADER: anything" \
+         "$DASHB_URL/api/saved_objects/index-pattern/${INDEX_PATTERN}" 2>/dev/null | \
+         jq -r '.attributes.fieldFormatMap' 2>/dev/null | \
+         jq -c 'with_entries(.value.params.parsedUrl? = null | del(.value.params.parsedUrl))' 2>/dev/null | \
+         jq '@json' >"$FIELD_FORMAT_MAP_FILE_TEMP" 2>/dev/null ) || true
+  FIELD_FORMAT_MAP_FILE_SIZE=$(stat -c%s "$FIELD_FORMAT_MAP_FILE_TEMP")
+
+  # Create index pattern (preserving custom field formatting)
+  INDEX_PATTERN_FILE_TEMP="$(mktemp)"
+  echo "{\"attributes\":{\"title\":\"$INDEX_PATTERN\",\"timeFieldName\":\"$INDEX_TIME_FIELD\"}}" > "$INDEX_PATTERN_FILE_TEMP"
+  if (( $FIELD_FORMAT_MAP_FILE_SIZE > 64 )); then
+    echo "Preserving existing field formatting for \"$INDEX_PATTERN\"..."
+    jq --slurpfile fieldFormatMap "$FIELD_FORMAT_MAP_FILE_TEMP" '.attributes.fieldFormatMap = $fieldFormatMap[0]' "$INDEX_PATTERN_FILE_TEMP" > "$INDEX_PATTERN_FILE_TEMP".new && \
+      mv "$INDEX_PATTERN_FILE_TEMP".new "$INDEX_PATTERN_FILE_TEMP"
+  fi
+  echo "Creating index pattern \"$INDEX_PATTERN\"..."
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" -H "$XSRF_HEADER: anything" \
+    "$DASHB_URL/api/saved_objects/index-pattern/${INDEX_PATTERN}?overwrite=${TEMPLATES_IMPORTED}" \
+    -d @"$INDEX_PATTERN_FILE_TEMP" 2>&1
+  rm -f "$INDEX_PATTERN_FILE_TEMP" "$FIELD_FORMAT_MAP_FILE_TEMP"
+
+  echo "Setting default index pattern..."
+
+  # Make it the default index
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL -XPOST -H "Content-Type: application/json" -H "$XSRF_HEADER: anything" \
+    "$DASHB_URL/api/$DASHBOARDS_URI_PATH/settings/defaultIndex" \
+    -d"{\"value\":\"$INDEX_PATTERN\"}"
+
+  # end Index pattern
+  #############################################################################################################################
+
+  #############################################################################################################################
+  # OpenSearch Tweaks
+  #   - TODO: only do these if they've NEVER been done before?
+  echo "Updating UI settings..."
+
+  # set dark theme (or not)
+  [[ "$DARK_MODE" == "true" ]] && DARK_MODE_ARG='{"value":true}' || DARK_MODE_ARG='{"value":false}'
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPOST "$DASHB_URL/api/$DASHBOARDS_URI_PATH/settings/theme:darkMode" \
+    -H "$XSRF_HEADER:true" -H 'Content-type:application/json' -d "$DARK_MODE_ARG" || true
+
+  # set default query time range
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPOST "$DASHB_URL/api/$DASHBOARDS_URI_PATH/settings" \
+    -H "$XSRF_HEADER:true" -H 'Content-type:application/json' \
+    -d '{"changes":{"timepicker:timeDefaults":"{\n  \"from\": \"now-24h\",\n  \"to\": \"now\",\n  \"mode\": \"quick\"}"}}' || true
+
+  # turn off telemetry
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPOST "$DASHB_URL/api/telemetry/v2/optIn" \
+    -H "$XSRF_HEADER:true" -H 'Content-type:application/json' \
+    -d '{"enabled":false}' || true
+
+  # pin filters by default
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPOST "$DASHB_URL/api/$DASHBOARDS_URI_PATH/settings/filters:pinnedByDefault" \
+      -H "$XSRF_HEADER:true" -H 'Content-type:application/json' \
+      -d '{"value":true}' || true
+
+  # enable in-session storage
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPOST "$DASHB_URL/api/$DASHBOARDS_URI_PATH/settings/state:storeInSessionStorage" \
+    -H "$XSRF_HEADER:true" -H 'Content-type:application/json' \
+    -d '{"value":true}' || true
+
+  echo "UI settings updates complete!"
+
+  # end OpenSearch Tweaks
+  #############################################################################################################################
+
+  # OpenSearch Create Initial Indices
+
+  curl "${CURL_CONFIG_PARAMS[@]}" -w "\n" -fsSL \
+    -XPUT "$OPENSEARCH_URL_TO_USE/${INDEX_PATTERN%?}initial" \
+    -H "$XSRF_HEADER:true" -H 'Content-type:application/json'
+
+  touch "${STARTUP_IMPORT_PERFORMED_FILE}"
+
+fi # dashboards is running
+
+echo "Success" >&2
