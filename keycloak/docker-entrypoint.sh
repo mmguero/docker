0ed7a9d6e84d1e22411785580ac395fe3aa63240
Seth Grover <seth.d.grover@gmail.com>
2025-03-10 11:04:47 -0600

    Added keycloak

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -2,17 +2,25 @@
 
 POSTGRES_HOST=${POSTGRES_HOST:-postgres}
 PGPORT=${PGPORT:-5432}
+POSTGRES_MAIN_DB=${POSTGRES_DB:-postgres}
 POSTGRES_KEYCLOAK_DB=${POSTGRES_KEYCLOAK_DB:-keycloak}
 export KC_DB_USERNAME="${POSTGRES_KEYCLOAK_USER:-keycloak}"
 export KC_DB_PASSWORD="${POSTGRES_KEYCLOAK_PASSWORD:-}"
 export KC_DB_URL="jdbc:postgresql://${POSTGRES_HOST}:${PGPORT}/${POSTGRES_KEYCLOAK_DB}"
 export KC_DB=postgres
-until PGPASSWORD="${KC_DB_PASSWORD}" pg_isready -h "${POSTGRES_HOST}" -p ${PGPORT} -U "${KC_DB_USERNAME}" >/dev/null 2>&1; do
+
+until PGPASSWORD="${KC_DB_PASSWORD}" pg_isready -U "${KC_DB_USERNAME}" \
+                                                -h "${POSTGRES_HOST}" -p ${PGPORT} >/dev/null 2>&1; do
   sleep 5
 done
-echo "PostgreSQL is up and ready at ${KC_DB_URL}"
+echo "PostgreSQL is responding..."
 
-# todo: remove and put in a proper check for the user/db
-sleep 20
+until PGPASSWORD="${KC_DB_PASSWORD}" psql -U "${KC_DB_USERNAME}" \
+                                          -h "${POSTGRES_HOST}" -p ${PGPORT} \
+                                          -d "${POSTGRES_MAIN_DB}" -tAc \
+    "SELECT 1 FROM pg_database WHERE datname = '${POSTGRES_KEYCLOAK_DB}';" 2>/dev/null | grep -q 1; do
+  sleep 5
+done
+echo "PostgreSQL is up and ready at ${KC_DB_URL}!"
 
 exec "$@"
